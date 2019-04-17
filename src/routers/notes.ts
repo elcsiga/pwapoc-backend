51d70aabae51ea299b8179c1a5f8632c1a9c4927
Andras Kovacs <Andras.Kovacs4@ge.com>
2019-04-17 17:13:51 +0200

    izy

--- src/routers/notes.ts
+++ src/routers/notes.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import {ServerError} from "../utils/error";
 import {Note} from "../common";
 
-
 const delay = 1200;
 
 export const notesRouter = express.Router();
@@ -17,7 +16,7 @@ notesRouter.get('/', (req, res) => {
 });
 
 notesRouter.post('/', (req, res) => {
-    const text = getText(req);
+    const text = getText(req.body.text);
     const id = getNextId();
     const note = {id, text, status: 'saved'};
     notes.push(note);
@@ -25,32 +24,69 @@ notesRouter.post('/', (req, res) => {
 });
 
 notesRouter.put('/:id', (req, res) => {
-    const text = getText(req);
-    const index = getIndex(req);
+    const text = getText(req.body.text);
+    const index = getIndex(+req.params.id);
     notes[index].text = text;
     notes[index].status = 'saved';
     setTimeout(() => res.json(notes[index]), delay);
 });
 
 notesRouter.delete('/:id', (req, res) => {
-    const index = getIndex(req);
+    const index = getIndex(+req.params.id);
     const note = notes[index];
     notes.splice(index,1);
     setTimeout(() => res.json(note), delay);
 });
 
-///
+//////////////////////////
 
-function getText(req): string {
-    if (req.body.text && typeof req.body.text === 'string' && req.body.text.length <= 10) {
-        return req.body.text;
+notesRouter.post('/sync', (req, res) => {
+
+    const notes = req.body.notes;
+    for (let note of notes) {
+
+        console.log('SYNC PROCESSING:', note);
+
+        // new element
+        if (note.status === 'cached' && note.id >= 1000) {
+            const text = getText(note.text);
+            const id = getNextId();
+            const newNote = {id, text, status: 'saved'};
+            notes.push(newNote);
+            console.log('NOTE ADDED:', newNote);
+        }
+
+        // deleted element
+        else if (note.status == 'deleted') {
+            const index = getIndex(note.id);
+            const notesDeleted = notes.splice(index,1);
+            console.log('NOTE DELETED:', notesDeleted);
+        }
+
+        // modified element
+        else if (note.status === 'cached') {
+            const text = getText(note.text);
+            const index = getIndex(note.id);
+            notes[index].text = text;
+            notes[index].status = 'saved';
+            console.log('NODE MODIFIED:', notes[index]);
+        }
+    }
+
+    setTimeout(() => res.json(notes), delay);
+});
+
+//////////////////////////
+
+function getText(text): string {
+    if (text && typeof text === 'string' && text.length <= 10) {
+        return text;
     } else {
         throw new ServerError(400, 'Invalid text');
     }
 }
 
-function getIndex(req): number {
-    const id = +req.params.id;
+function getIndex(id): number {
     const index = notes.findIndex(n => n.id === id);
     if (index => 0) {
         return index;
